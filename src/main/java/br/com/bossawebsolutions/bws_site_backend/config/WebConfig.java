9092c37adcf28d5750b5041bd4d42b1d1564a557
Guilherme Bossa <gvfbossa@hotmail.com>
2024-10-23 16:46:36 -0300

    Configured allowed origins and methods

--- src/main/java/br/com/bossawebsolutions/bws_site_backend/config/WebConfig.java
+++ src/main/java/br/com/bossawebsolutions/bws_site_backend/config/WebConfig.java
@@ -1,6 +1,5 @@
 package br.com.bossawebsolutions.bws_site_backend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("https://gvfbossa.github.io/bws-website/*")
-                .allowedMethods("POST")
+                .allowedMethods("POST", "OPTIONS")
                 .allowedHeaders("*");
     }
 }
-
-//TODO testar com postman
