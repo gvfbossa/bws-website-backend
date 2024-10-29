f53128e3f5d5adc3bd6239c383498421765a72b8
Guilherme Bossa <gvfbossa@hotmail.com>
2024-10-29 11:07:59 -0300

    V1 email config

--- src/main/java/br/com/bossawebsolutions/bws_site_backend/service/EmailService.java
+++ src/main/java/br/com/bossawebsolutions/bws_site_backend/service/EmailService.java
@@ -1,6 +1,7 @@
 package br.com.bossawebsolutions.bws_site_backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,12 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
+    @Value("${EMAIL}")
+    private String fromEmail;
+
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
